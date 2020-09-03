23ad069ee4cc3ebf02c4e859e7b4d07cd137d142
michaelabajerova <bajerova.misa@seznam.cz>
2020-09-04 00:55:45 +0200

    new content available

---
+++ week1/day4/CVM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CVM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 24;
+            int out = 0;
+
+
+            Console.WriteLine(a++);
+        }
+    }
+}
---
+++ week1/day4/IWontCheat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IWontCheat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("I won't cheat on exam", i);
+            }
+
+        }
+    }
+}
