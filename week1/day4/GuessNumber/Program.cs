d54ff7c1e22d2bca2200b48649c89f551733c925
michaelabajerova <bajerova.misa@seznam.cz>
2020-09-04 17:13:27 +0200

    new content

---
+++ week1/day4/GuessNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace guessnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+            int ourNumber = 10;
+
+            if (usersNumber > ourNumber)
+                {
+                Console.WriteLine("should be lower");
+            }
+            if (usersNumber < ourNumber)
+            {
+                Console.WriteLine("should be higher");
+            }
+            if (usersNumber == ourNumber)
+            {
+                Console.WriteLine("you found number 10");
+            }
+            Console.ReadLine();
+        } 
+        
+    }
+}
---
+++ week1/day4/OneTwoALot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OneTwoALot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (a <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            if (a == 1)
+            {
+                Console.WriteLine("One");
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            if (a > 2)
+            {
+                Console.WriteLine("A Lot");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
