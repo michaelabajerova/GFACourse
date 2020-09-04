b7510fc261777047d9d46535fd2358de6caffe9c
michaelabajerova <bajerova.misa@seznam.cz>
2020-09-04 17:09:17 +0200

    new content

---
+++ week1/day4/Triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Trinagle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int row = 1; row < 8; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
