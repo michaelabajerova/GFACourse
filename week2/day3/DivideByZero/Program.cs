a4a467155973859f5ea0246b46554354cc113e05
michaelabajerova <bajerova.misa@seznam.cz>
2020-09-09 14:19:36 +0200

    File Manipulation Practise

---
+++ week2/day3/DivideByZero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // It should print "fail" if the parameter is 0
+            try
+            {
+                Console.WriteLine("Enter number:");
+                int number = int.Parse(Console.ReadLine());
+                int result = 10 / number;
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fail");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
