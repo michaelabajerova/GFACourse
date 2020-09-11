d34d0b916f6a8f1faa1642d3b9595c9940aecbf6
michaelabajerova <bajerova.misa@seznam.cz>
2020-09-11 17:32:05 +0200

    Project

--- week2/day5/LinePLay/MainWindow.axaml.cs
+++ week2/day5/LinePLay/MainWindow.axaml.cs
@@ -22,7 +22,9 @@ namespace DrawingApplication
             var foxDraw = new FoxDraw(canvas);
             foxDraw.SetBackgroundColor(Colors.Black);
 
-            linePlay(foxDraw);
+            /*linePlay(foxDraw);*/
+            /*linePlay4(foxDraw);*/
+            linePlay16(foxDraw);
         }
 
         public static void linePlay(FoxDraw foxDraw)
@@ -42,6 +44,75 @@ namespace DrawingApplication
             for (int i = 0; i < 17; i++)
             {
                 foxDraw.DrawLine(0 + (i * 50), 0, 800, 10 + (i * 50));
+            }
+        }
+        /// Don't kill me for this code ///
+        public static void linePlay4(FoxDraw foxDraw)
+        {
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(0, 400);
+            foxDraw.DrawLine(startPoint, endPoint);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(0, 0 + (i * 40), 0 + (i * 40), 400);
+                foxDraw.SetStrokeColor(Colors.DarkViolet);
+            }
+            startPoint = new Point(0, 0);
+            endPoint = new Point(400, 0);
+            foxDraw.DrawLine(startPoint, endPoint);
+            foxDraw.SetStrokeColor(Colors.AntiqueWhite);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(0 + (i * 40), 0, 400, 0 + (i * 40));
+            }
+
+            startPoint = new Point(400, 0);
+            endPoint = new Point(400, 400);
+            foxDraw.DrawLine(startPoint, endPoint);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(400, 0 + (i * 40), 400 + (i * 40), 400);
+                foxDraw.SetStrokeColor(Colors.Blue);
+            }
+            startPoint = new Point(400, 0);
+            endPoint = new Point(800, 0);
+            foxDraw.DrawLine(startPoint, endPoint);
+            foxDraw.SetStrokeColor(Colors.IndianRed);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(400 + (i * 40), 0, 800, 0 + (i * 40));
+            }
+            startPoint = new Point(0, 400);
+            endPoint = new Point(0, 800);
+            foxDraw.DrawLine(startPoint, endPoint);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(0, 400 + (i * 40), 0 + (i * 40), 800);
+                foxDraw.SetStrokeColor(Colors.BurlyWood);
+            }
+            startPoint = new Point(0, 400);
+            endPoint = new Point(400, 400);
+            foxDraw.DrawLine(startPoint, endPoint);
+            foxDraw.SetStrokeColor(Colors.Aquamarine);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(0 + (i * 40), 400, 400, 400 + (i * 40));
+            }
+            startPoint = new Point(400, 400);
+            endPoint = new Point(400, 800);
+            foxDraw.DrawLine(startPoint, endPoint);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(400, 400 + (i * 40), 400 + (i * 40), 800);
+                foxDraw.SetStrokeColor(Colors.Firebrick);
+            }
+            startPoint = new Point(400, 400);
+            endPoint = new Point(800, 400);
+            foxDraw.DrawLine(startPoint, endPoint);
+            foxDraw.SetStrokeColor(Colors.LawnGreen);
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.DrawLine(400 + (i * 40), 400, 800, 400 + (i * 40));
             }
         }
 
